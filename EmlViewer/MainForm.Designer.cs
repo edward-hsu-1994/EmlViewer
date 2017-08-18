be4c1337bfdd88bbfcc00004cbb0918645e114fa
徐培堯 <st89382000@gmail.com>
2017-08-18 16:28:15 +0800

    實作EML檔案讀取

---
+++ EmlViewer/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EmlViewer {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.說明HToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.關於AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開啟舊檔OToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.結束XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.htmlViewer = new System.Windows.Forms.WebBrowser();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.說明HToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(736, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開啟舊檔OToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.結束XToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(58, 20);
+            this.toolStripMenuItem1.Text = "檔案(&F)";
+            // 
+            // 說明HToolStripMenuItem
+            // 
+            this.說明HToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.關於AToolStripMenuItem});
+            this.說明HToolStripMenuItem.Name = "說明HToolStripMenuItem";
+            this.說明HToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.說明HToolStripMenuItem.Text = "說明(&H)";
+            // 
+            // 關於AToolStripMenuItem
+            // 
+            this.關於AToolStripMenuItem.Name = "關於AToolStripMenuItem";
+            this.關於AToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.關於AToolStripMenuItem.Text = "關於(&A)";
+            // 
+            // 開啟舊檔OToolStripMenuItem
+            // 
+            this.開啟舊檔OToolStripMenuItem.Name = "開啟舊檔OToolStripMenuItem";
+            this.開啟舊檔OToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.開啟舊檔OToolStripMenuItem.Text = "開啟舊檔(&O)";
+            this.開啟舊檔OToolStripMenuItem.Click += new System.EventHandler(this.開啟舊檔OToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // 結束XToolStripMenuItem
+            // 
+            this.結束XToolStripMenuItem.Name = "結束XToolStripMenuItem";
+            this.結束XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.結束XToolStripMenuItem.Text = "結束(&X)";
+            this.結束XToolStripMenuItem.Click += new System.EventHandler(this.結束XToolStripMenuItem_Click);
+            // 
+            // htmlViewer
+            // 
+            this.htmlViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlViewer.Location = new System.Drawing.Point(0, 24);
+            this.htmlViewer.MinimumSize = new System.Drawing.Size(20, 20);
+            this.htmlViewer.Name = "htmlViewer";
+            this.htmlViewer.Size = new System.Drawing.Size(736, 389);
+            this.htmlViewer.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 413);
+            this.Controls.Add(this.htmlViewer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 開啟舊檔OToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 結束XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 說明HToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 關於AToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser htmlViewer;
+    }
+}
---
+++ EmlViewer/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmlViewer {
+    public partial class MainForm : Form {
+        public MainForm(string filePath = null) {
+            InitializeComponent();
+
+            if (filePath != null) {
+                LoadFile(filePath);
+            }
+        }
+
+        public void LoadFile(string filePath) {
+            var template = File.ReadAllText(@"assets\template.html");
+            var message = MimeMessage.Load("D:\\test.eml");
+
+            var varList = typeof(MimeMessage).GetProperties();
+
+            foreach (var pro in varList) {
+                template = template
+                    .Replace("{{" + pro.Name + "}}", pro.GetValue(message) as string);
+            }
+
+            htmlViewer.DocumentText = template;
+        }
+
+        private void 結束XToolStripMenuItem_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+
+        private void 開啟舊檔OToolStripMenuItem_Click(object sender, EventArgs e) {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Multiselect = true;
+            op.Filter = "電子郵件檔案 (*.eml)|*.eml";
+            if (op.ShowDialog() != DialogResult.OK) return;
+
+            if (op.FileNames.Length == 1) {
+                LoadFile(op.FileNames[0]);
+            } else {
+                LoadFile(op.FileNames[0]);
+                foreach (var file in op.FileNames.Skip(1)) {
+                    new MainForm(file).Show();
+                }
+            }
+        }
+    }
+}
